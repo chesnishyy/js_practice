907c009e18194fed97c979ebf0f2acdca42eeee7
Igor Chesnishyy <i.chesnishyy@gmail.com>
2018-04-03 17:01:34 +0300

    Fixed readme file

--- lesson_1.js
+++ lesson_1.js
@@ -3,38 +3,64 @@ import * as sw_api from './sw_api';
 //1. LESSON 1
 
 	//1.1 for loop
-	function getFormatedFilms(films) {
-		let counter,
-			result = [];
+		function getFormatedFilms(films) {
+			let counter,
+				result = [];
 
-		for (counter = 0; counter < films.length; counter++) {
-			result.push({
-				episode: films[counter].episode_id,
-				name: films[counter].title,
-				director: films[counter].director
-			})
+			for (counter = 0; counter < films.length; counter++) {
+				result.push({
+					episode: films[counter].episode_id,
+					name: films[counter].title,
+					director: films[counter].director
+				})
+			}
+			return result;
 		}
-		return result;
-	}
 
-	// 1.2 forEach
-	function getFormatedFilms(films) {
-		let result = [];
+	// 1.2 About forEach:
+		/**
+		 * Array.prototype.forEach ( callbackfn [ , thisArg ] )
+		 * 
+		 * 
+			callbackfn should be a function that accepts three arguments. forEach calls callbackfn once for each element present in the array, in ascending order.
+			callbackfn is called only for elements of the array which actually exist; it is not called for missing elements of the array.
 
-		films.forEach((film) => {
-			result.push({
-				episode: film.episode_id,
-				name: film.title,
-				director: film.director,
-				release: firm.release_date
+			If a thisArg parameter is provided, it will be used as the this value for each invocation of callbackfn. If it is not provided, undefined is used instead.
+
+			callbackfn is called with three arguments: the value of the element, the index of the element, and the object being traversed.
+
+			forEach does not directly mutate the object on which it is called but the object may be mutated by the calls to callbackfn.
+		* 
+		* @param callbackfn [ , thisArg ]
+		* @returns undefined
+		*/
+
+	// 1.3 forEach simple example
+		function getFormatedFilms(films) {
+			let result = [];
+
+			films.forEach((film) => {
+				result.push({
+					episode: film.episode_id,
+					name: film.title,
+					director: film.director,
+					release: firm.release_date
+				})
 			})
-		})
-	
-		return result;
+		
+			return result;
+		}
+
+	// 1.4 forEach example with two callback arguments
+	function someFunction(films) {
+		
 	}
 
-	// 1.3 
-	function
+	// 1.5 forEach advanced example with [thisArg]
+	function someAdvancedFunction(films) {
+		
+	}
+
 
 
 sw_api.getAllFilms()
@@ -42,7 +68,8 @@ sw_api.getAllFilms()
 
 		//There is an Array with full list of Star Wars films in variable films
 		const films = response.results;
-		console.log(films[0])
+
+
 		// 1.1
 		///console.log(getFormatedFilms(films));
 		
---
+++ readme.md
@@ -0,0 +1,34 @@
+#JS Practice course plan
+    1. Lesson 1. Introduction. The Array forEach method
+        1.1 Introduction ( ~2 minutes)
+            1.1.1 Intro speach
+            1.1.2 What we will learn in this course
+            1.1.3 Our goal, and for whom this course is
+            1.1.4 Course structure, hw task at the end of every lesson
+        1.2 for loop example
+        1.3 About forEach - small theoretical part
+        1.4 forEach simple example
+        1.5 forEach example with two callback arguments
+        1.6 forEach advanced example with [thisArg] argument
+        1.7 hw_1
+    2. Lesson 2. The Array map method
+        2.1 Do hw_1 task
+        2.2 Get new array with forEach
+        2.3 About map method - small theoretical part
+        2.4 map simple example
+        2.5 map example with two callback arguments
+        2.6 map advanced example with [thisArg] argument
+        2.7 hw_2
+    3. Lesson 3. The Array filter method
+
+    4. Lesson 4. The Array some/every methods
+
+    5. Lesson 5. The Array reduce/reduceRight methods
+
+    6. Lesson 6. Chaining the Array map, filter and forEach
+
+    7. Lesson 7. Promises. Fetch. SW API
+
+    8. Lesson 8. Project init, add and install modules
+
+    9. 
